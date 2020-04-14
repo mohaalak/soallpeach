a94785180beb783cda5ed0faa1cd8a1bb65d709a
Mohammad Hadi Aliakbar <hadi.aliakbar@gmail.com>
2020-04-14 20:09:38 +0430

    fix: remove ramda

--- prime/src/index.js
+++ prime/src/index.js
@@ -1,25 +1,21 @@
 const fs = require('fs')
-const { exec } = require('child_process')
 const assert = require('assert')
-const R = require('ramda')
 const isPrime = require('./isPrime')
 const { memoize1 } = require('./lib/utility')
 
 const calcFunction = memoize1(isPrime)
-
-const lines = R.join('\n')
-const unlines = R.split('\n')
-
-const start = R.pipe(unlines, R.map(calcFunction), lines)
 
 function main () {
   const filePath = process.argv[2]
 
   assert(filePath, 'you sould provide a file')
 
-  const data = fs.readFileSync(filePath, { encoding: 'utf8' })
-  const result = start(data)
+  const data = fs
+    .readFileSync(filePath, { encoding: 'utf8' })
+    .split('/n')
+    .map(calcFunction)
+    .join('/n')
 
-  process._rawDebug(result)
+  process.stdout.write(data)
 }
 main()
